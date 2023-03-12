47c0ecf78b96077ab73d2aa2bb6bb14b999a0b37
JenniferEinstein <jennifereinstein@gmail.com>
2023-03-12 17:25:14 -0400

    changes made 3/12. Commit before push.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
+
 import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from './components/common/About';
-// import Footer from './components/common/About';
-// import Nav from './components/common/Nav';
+import Footer from './components/common/Footer';
+ import Nav from './components/common/Nav';
 // import ErrorMessage from './components/errors/ErrorMessage';
-// import Home from './components/home/Home';
+ import Home from './components/home/Home';
+//  import { Link, NavLink } from 'react-router-dom';
 // import View from './components/video/View';
 
 
 function App() {
+ 
   return (
-    <About />
+    <div className="App">
+      <header>
+        "Header"  
+    {/* <Header /> */}
+        <Nav />
+      </header>
+    <About /> 
+    <main>
+      <h1>CJR You Tube Clone Class Project</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+      </Routes>
+    </main>
+    <div className="footer">
+      <Footer />
+    </div>
+
+    </div>
+    
   )
 }
 
