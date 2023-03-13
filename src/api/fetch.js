5c3793c8fb0420a6b938f4319d9fe6217feee919
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-03-13 19:56:20 -0400

    .env deletion replaced with BASE_URL in fetch.js

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,13 +1,16 @@
-//const URL = process.env.REACT_APP_API_BASE_URL;
+
+const API_KEY = `AIzaSyCTA2H0QujmwEucZg81o_5EovQ77oCKZ2A`
+const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=10&key=${API_KEY}`
+const searchTerm = 
 
 //to GET all the videos listing index being searched for.
-
-export function getVideos() {
-  return fetch(`${URL}/videos`).then((response) => response.json());
+export function getQueryVideos(searchTerm) {
+    return fetch(`BASE_URL`).then((response) => response.json())
 }
+//! SHOULD WE ADD A PATH TO THIS 
 
 
 //To GET one video after selecting from the listing.
 export function getOneVideo(id) {
-  return fetch(`${URL}/video/${id}`).then((response) => response.json());
+  return fetch(`${BASE_URL}/video/${id}`).then((response) => response.json());
 }
