268ac1bfd2a42a720998c768804be4b44b9c9928
JenniferEinstein <jennifereinstein@gmail.com>
2023-03-14 19:55:58 -0400

    Add required short bio of each person but feel free to change. Also add required links to each person's GitHub page.

--- src/components/common/About.js
+++ src/components/common/About.js
@@ -3,12 +3,21 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-            <h1>ABOUT</h1>
-            <p>  <h3>Hello Welcome to Out YouTube Clone Project!</h3>
-      <p>Ray Russell, Jennifer Einstein & Christina Cephus present our Best!</p>
-            </p>
+      <h1>ABOUT</h1>
+          <h3>Hello Welcome to Out YouTube Clone Project!</h3>
+          <p>Ray Russell, Jennifer Einstein & Christina Cephus present our Best!</p>
 
-      <p> Give our regards to Broadway!</p>
+<br></br>
+    <p><span><b>Christina Cephus</b></span> prides herself on solving modern technical complexity with pragmatic creativity. She is the founder of Low Key High Tech News.</p>
+    <p> https://github.com/theCephusHasLanded</p>
+
+    <p> Hailing from the Golden State, <span><b>Jennifer Einstein</b></span>  moved to New York over 20 years ago and never looked back. She aims to make the modern world a better place, one line of code at a time.</p>
+    <p>  https://github.com/JenniferEinstein</p>
+     
+
+    <p><span><b>Ray Russell</b></span> is the owner of Rayology, a lifestyle and wellness venture. </p>
+    <p>https://github.com/RadiantRay22</p>
+
             
     </div>
   )
