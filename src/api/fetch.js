1bd679331f82b1f63416f206db9bac390e741651
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-03-14 21:56:48 -0400

    Fetch is working -- and refactored home component to display search

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,31 +1,27 @@
 import axios from 'axios';
-//import ErrorMessage from '../components/errors/ErrorMessage';
+const API_KEY = 'AIzaSyCTA2H0QujmwEucZg81o_5EovQ77oCKZ2A';
 
-const API_KEY = `AIzaSyCTA2H0QujmwEucZg81o_5EovQ77oCKZ2A`
-const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=10&key=${API_KEY}`
+//To GET all the videos listing index being searched for.
 
-//to GET all the videos listing index being searched for.
-
-export async function getQueryVideos(searchTerm) {
-    try{
-        const response = await axios(`BASE_URL`)
-        console.log(response)
-        setVideos(response.data);
-    } catch(error){
-        console.log(error)
-    }
-    
+export async function getQueryVideos(query) {
+  const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?q=${query}&part=snippet&maxResults=10&key=${API_KEY}`;
+  try {
+    const response = await axios(BASE_URL);
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
-//! SHOULD WE ADD A PATH TO THIS 
-
 
 //To GET one video after selecting from the listing.
 export async function getOneVideo(id) {
-    try{
-        const response = await axios (`${BASE_URL}/video/${id}`)
-        console.log = (response)
-        setVideo(response.data);
-    } catch(error){
-        console.log(error)
-    }
+  const BASE_URL = `https://youtube.googleapis.com/youtube/v3/videos?id=${id}&part=snippet&key=${API_KEY}`
+  try {
+    const response = await axios(BASE_URL);
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error)
+  }
 }
--- src/components/landing/Home.js
+++ src/components/landing/Home.js
@@ -1,49 +1,33 @@
-// import View from '../video/View'
-// import React, { useState } from 'react';
-import { getQueryVideos } from "../../api/fetch"
+import React, { useState } from 'react';
+import { getQueryVideos } from '../../api/fetch';
+import { getOneVideo } from '../../api/fetch';
 
 const Home = () => {
+    const [query, setQuery] = useState('');
 
-// const [videos, setVideos] = useState("")
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        getQueryVideos(query);
+    };
 
-
-
-
-// function Search( {onSubmit }) {
-//   const [query, setQuery] = useState("")
-
-// const handleSubmit = (event) => {
-// event.preventDefault();
-// onSubmit(query);
-// }
-
-// const handleChange =(event) => {
-//   setQuery(event.target.value)
-// }
+    const handleChange = (event) => {
+        setQuery(event.target.value)
+    }
 
 
-// Ray's form
+return (
+    <div>
+        <h1>WE ARE HOME</h1>
+        <h2>Search Youtube Below</h2>
+        <br />
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="VideoSearch"></label>
+            <input type="text" placeholder="Search" value={query} 
+            onChange={handleChange} />
+            <input type="submit" id="Query" name="Search" value="SEARCH" />
+        </form>
+    </div>
+)
+};
 
-/* return(
- <div>
-<form>
- <label for=VideoSearch></label>
- <input type = "text" placeholder= Search >
- <input type="submit" id="Query" name="Search" value={query}><br>
-</form>
-</div> */
-
-//   return (
-//     <div>
-//       <h1>WE ARE HOME</h1>
-//       <p>Search Youtube Below</p><br/>
-//       <form onSubmit={handleSubmit}> 
-//       <input type="text" value={query} onChange={handleChange} />
-//       <button type="submit">SEARCH</button>
-//       </form>
-//     </div>
-//   )
-// }
-}
-
-export default Home
+export default Home;
