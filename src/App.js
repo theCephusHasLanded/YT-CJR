b8adeb0d06a854b5769e9e4dfc964ffc7ca1fbb6
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-03-16 00:06:50 -0400

    added climate crisis font, refactored navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import About from './components/common/About';
 import Footer from './components/common/Footer';
 import Nav from './components/common/Nav';
