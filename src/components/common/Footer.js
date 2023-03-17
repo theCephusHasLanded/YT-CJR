6c0f725ff3ec2b33177ef693a40aa0962d89dcc6
JenniferEinstein <jennifereinstein@gmail.com>
2023-03-16 20:48:52 -0400

    Create component for footer css. CSS for Footer that keeps the footer at the bottom of the page

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { CLink } from '@coreui/react';
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      <h2>These are the Feet</h2>
+    <div className="footer">
+      <CLink href="https://github.com/theCephusHasLanded/YT-CJR">Our Github Repository</CLink>
+      <div className="footer__center">
+        <span>&copy; 2023 An Untamed Glories  |  LKHN  |  Rayology Collaboration</span>
+      </div>
+      <span>Powered by </span>
+      <CLink href="https://www.pursuit.org/">Pursuit</CLink>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
