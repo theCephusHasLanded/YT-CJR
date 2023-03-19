d67baaa9c2bfb8338b1eed1a6090a720bcd6a65c
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-03-18 23:36:21 -0400

    3's a charm

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -12,7 +12,6 @@ const Footer = () => {
       <div className='footer__left'>
       <span>Powered by</span>
       <CLink href="https://www.pursuit.org/">Pursuit</CLink>
-      <a href="https://icons8.com/icon/DI6zcEak7cFp/youtube-logo">YouTube Logo</a> icon by <a href="https://icons8.com">Icons8</a>
       </div>
     </div>
   );
